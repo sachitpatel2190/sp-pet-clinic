c97fd12a7461c28f8898c68a2a5271b94cf0c67d
sachit <sachitp2190@gmail.com>
2020-05-21 00:25:04 +0530

    First commit

---
+++ src/main/java/com/sachit/learn/sppetclinic/spring/Application.java
@@ -0,0 +1,13 @@
+package com.sachit.learn.sppetclinic.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
